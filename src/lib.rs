70e7662180b6488353c6b40edcaae35d86027a3b
Robert Jackson <me@rwjblue.com>
2023-12-28 18:19:28 -0500

    More tests, get some basic stuff working.
    Still not working fully though. Can't use both files and directories
    yet.

--- src/lib.rs
+++ src/lib.rs
@@ -51,17 +51,6 @@ impl PartialEq for DirectoryContent {
 
 #[macro_export]
 macro_rules! directory {
-    // Recursive directory structure
-    //( $( $key:expr => { $( $inner_key:expr => $inner_value:tt ),* $(,)? } ),* $(,)? ) => {
-    //    {
-    //        let mut map = std::collections::HashMap::new();
-    //        $(
-    //            map.insert($key.to_string(), DirectoryContent::Directory(Directory::new(directory! { $( $inner_key => $inner_value ),* })));
-    //        )*
-    //        DirectoryContent::Directory(Directory(map))
-    //    }
-    //};
-
     // Literal file entry
     ( $( $key:expr => $value:expr ),* $(,)? ) => {
         {
@@ -72,6 +61,17 @@ macro_rules! directory {
             DirectoryContent::Directory(Directory(map))
         }
     };
+    // Recursive directory structure
+    ( $( $key:expr => { $( $inner_key:expr => $inner_value:tt ),* $(,)? } ),* $(,)? ) => {
+        {
+            let mut map = std::collections::HashMap::new();
+            $(
+                map.insert($key.to_string(), directory! { $( $inner_key => $inner_value ),* });
+            )*
+            DirectoryContent::Directory(Directory(map))
+        }
+    };
+
 }
 
 #[cfg(test)]
@@ -79,29 +79,13 @@ mod tests {
     use super::*;
 
     #[test]
-    fn fixture_macro_works() {
-        //let macro_fixture = fixture! {
-        //    "someDir" => directory! {
-        //        "someFile" => DirectoryContent::File("contents of file here".to_string()),
-        //    },
-        //    "someOtherFile" => DirectoryContent::File("other contents".to_string())
-        //};
+    fn directory_macro_works_for_files() {
         let macro_fixture = directory! {
             "someOtherFile" => "other contents",
             "otherFile" => "lol yesss",
         };
 
-        //let mut sub_directory_contents: HashMap<String, DirectoryContent> = HashMap::new();
-        //sub_directory_contents.insert(
-        //    "someFile".to_string(),
-        //    DirectoryContent::File("contents of file here".to_string()),
-        //);
-
         let mut contents = HashMap::new();
-        //contents.insert(
-        //    "someDir".to_string(),
-        //    DirectoryContent::Directory(Directory(sub_directory_contents)),
-        //);
         contents.insert(
             "someOtherFile".to_string(),
             DirectoryContent::File("other contents".to_string()),
@@ -111,6 +95,48 @@ mod tests {
             DirectoryContent::File("lol yesss".to_string()),
         );
 
+        let expected = DirectoryContent::Directory(Directory(contents));
+
+        assert_eq!(macro_fixture, expected);
+    }
+
+    #[test]
+    fn fixture_macro_works_for_empty_directories() {
+        let macro_fixture = directory! {
+            "someDir" => { },
+        };
+
+        let sub_directory_contents: HashMap<String, DirectoryContent> = HashMap::new();
+
+        let mut contents = HashMap::new();
+        contents.insert(
+            "someDir".to_string(),
+            DirectoryContent::Directory(Directory(sub_directory_contents)),
+        );
+        let expected = DirectoryContent::Directory(Directory(contents));
+
+        assert_eq!(macro_fixture, expected);
+    }
+
+    #[test]
+    fn fixture_macro_works_for_a_file_and_a_directory() {
+        let macro_fixture = directory! {
+            "someDir" => { },
+            "someFile" => "file contents here",
+        };
+
+        let sub_directory_contents: HashMap<String, DirectoryContent> = HashMap::new();
+
+        let mut contents = HashMap::new();
+        contents.insert(
+            "someFile".to_string(),
+            DirectoryContent::File("file contents here".to_string()),
+        );
+
+        contents.insert(
+            "someDir".to_string(),
+            DirectoryContent::Directory(Directory(sub_directory_contents)),
+        );
         let expected = DirectoryContent::Directory(Directory(contents));
 
         assert_eq!(macro_fixture, expected);
