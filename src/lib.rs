ce490a20edfa7b6b6ad81e4e0aa3c007b09dffcc
Robert Jackson <me@rwjblue.com>
2023-12-29 11:34:44 -0500

    Try using other techniques
    Mostly trying to reason through how json! works in serde

--- src/lib.rs
+++ src/lib.rs
@@ -27,27 +27,62 @@ impl PartialEq for DirectoryContent {
 
 #[macro_export]
 macro_rules! directory {
-    // Literal file entry
-    ( $( $key:expr => $value:expr ),* $(,)? ) => {
-        {
-            let mut map = std::collections::HashMap::new();
-            $(
-                map.insert($key.to_string(), DirectoryContent::File($value.to_string()));
-            )*
-            DirectoryContent::Directory(Directory(map))
-        }
-    };
-    // Recursive directory structure
-    ( $( $key:expr => { $( $inner_key:expr => $inner_value:tt ),* $(,)? } ),* $(,)? ) => {
-        {
-            let mut map = std::collections::HashMap::new();
-            $(
-                map.insert($key.to_string(), directory! { $( $inner_key => $inner_value ),* });
-            )*
-            DirectoryContent::Directory(Directory(map))
-        }
+    // argument order:
+    // 1. map
+    // 3. remaining tokens
+
+    // @directory
+    // empty directory entry
+    (@directory $map:ident ()) => {};
+
+    // // Next value is a directory.
+    // (@directory $map:ident ($($key:expr)+) (=> {$($dir:tt)*} $($rest:tt)*)) => {
+    //     let next_value = directory!({ $($dir)* });
+    //     let _ = $map.insert(($($key)+).into(), next_value);
+    //     directory!(@directory $map [$($key)+] (directory!({$($dir)*})) $($rest)*);
+    // };
+
+    // Next value is a string followed by a comma
+    (@directory $map:ident ($($key:expr)+) (=> $value:expr, $($rest:tt)*)) => {
+        let _ = $map.insert(($($key)+).into(), DirectoryContent::File($value.into()));
+
+        //directory!(@directory $map ($($rest)*));
     };
 
+    // // Recursive directory structure
+    // ( $( $key:expr => { $( $inner_key:expr => $inner_value:tt ),* $(,)? } ),* $(,)? ) => {
+    //     {
+    //         let mut map = std::collections::HashMap::new();
+    //         $(
+    //             map.insert($key.to_string(), directory! { $( $inner_key => $inner_value ),* });
+    //         )*
+    //         DirectoryContent::Directory(Directory(map))
+    //     }
+    // };
+    //
+    // // Literal file entry
+    // ( $( $key:expr => $value:expr ),* $(,)? ) => {
+    //     {
+    //         let mut map = std::collections::HashMap::new();
+    //         $(
+    //             map.insert($key.to_string(), DirectoryContent::File($value.to_string()));
+    //         )*
+    //         DirectoryContent::Directory(Directory(map))
+    //     }
+    // };
+
+    // handle empty invocation:
+    // directory! {}
+     () => {{
+         let mut map: HashMap<String, DirectoryContent> = std::collections::HashMap::new();
+         DirectoryContent::Directory(Directory(map))
+     }};
+
+    ({$($tt:tt)+}) => {{
+        let mut map = std::collections::HashMap::new();
+
+        directory!(@directory map ($($tt)+));
+    }};
 }
 
 #[cfg(test)]
@@ -77,44 +112,54 @@ mod tests {
     }
 
     #[test]
-    fn fixture_macro_works_for_empty_directories() {
-        let macro_fixture = directory! {
-            "someDir" => { },
-        };
+    fn fixture_macro_without_contents_works() {
+        let macro_fixture = directory! {};
 
-        let sub_directory_contents: HashMap<String, DirectoryContent> = HashMap::new();
-
-        let mut contents = HashMap::new();
-        contents.insert(
-            "someDir".to_string(),
-            DirectoryContent::Directory(Directory(sub_directory_contents)),
-        );
+        let contents = HashMap::new();
         let expected = DirectoryContent::Directory(Directory(contents));
 
         assert_eq!(macro_fixture, expected);
     }
 
-    #[test]
-    fn fixture_macro_works_for_a_file_and_a_directory() {
-        let macro_fixture = directory! {
-            "someDir" => { },
-            "someFile" => "file contents here",
-        };
+    // #[test]
+    // fn fixture_macro_works_for_empty_directories() {
+    //     let macro_fixture = directory! {
+    //         "someDir" => { },
+    //     };
+    //
+    //     let sub_directory_contents: HashMap<String, DirectoryContent> = HashMap::new();
+    //
+    //     let mut contents = HashMap::new();
+    //     contents.insert(
+    //         "someDir".to_string(),
+    //         DirectoryContent::Directory(Directory(sub_directory_contents)),
+    //     );
+    //     let expected = DirectoryContent::Directory(Directory(contents));
+    //
+    //     assert_eq!(macro_fixture, expected);
+    // }
 
-        let sub_directory_contents: HashMap<String, DirectoryContent> = HashMap::new();
-
-        let mut contents = HashMap::new();
-        contents.insert(
-            "someFile".to_string(),
-            DirectoryContent::File("file contents here".to_string()),
-        );
-
-        contents.insert(
-            "someDir".to_string(),
-            DirectoryContent::Directory(Directory(sub_directory_contents)),
-        );
-        let expected = DirectoryContent::Directory(Directory(contents));
-
-        assert_eq!(macro_fixture, expected);
-    }
+    // #[test]
+    // fn fixture_macro_works_for_a_file_and_a_directory() {
+    //     let macro_fixture = directory! {
+    //         "someDir" => { },
+    //         "someFile" => "file contents here",
+    //     };
+    //
+    //     let sub_directory_contents: HashMap<String, DirectoryContent> = HashMap::new();
+    //
+    //     let mut contents = HashMap::new();
+    //     contents.insert(
+    //         "someFile".to_string(),
+    //         DirectoryContent::File("file contents here".to_string()),
+    //     );
+    //
+    //     contents.insert(
+    //         "someDir".to_string(),
+    //         DirectoryContent::Directory(Directory(sub_directory_contents)),
+    //     );
+    //     let expected = DirectoryContent::Directory(Directory(contents));
+    //
+    //     assert_eq!(macro_fixture, expected);
+    // }
 }
